a049c71dab925c9497a7243ce565a12a3a090f7b
JalapenosBud <Jalapenosbud@gmail.com>
2018-11-04 13:44:17 +0100

    more

--- src/com/company/FlyweightPattern/FlyweightPatternMain.java
+++ src/com/company/FlyweightPattern/FlyweightPatternMain.java
@@ -6,8 +6,9 @@ public class FlyweightPatternMain
     
     public static void main(String[] args)
     {
-        //lav 10.000 objekter
-        for(int i=0; i < 10000; ++i) {
+        long startTime = System.currentTimeMillis();
+        //lav 100.000.000 objekter
+        for(int i=0; i < 500000; ++i) {
             //her vil vi lave et nyt objekt
             //men i stedet for at bruge new, så forespørger vi bare vores factory om det eksisterer
             Circle circle = (Circle)ShapeFactory.getCircle(getRandomColor());
@@ -19,6 +20,11 @@ public class FlyweightPatternMain
             //"tern" her printer vi bare til konsol
             circle.draw();
         }
+    
+        long endTime = System.currentTimeMillis();
+    
+        System.out.println("In FLYWEIGHT that took: " +  (endTime-startTime) + "ms");
+        
     }
     private static String getRandomColor() {
         return colors[(int)(Math.random()*colors.length)];
--- src/com/company/RegularImplementation/MainTest.java
+++ src/com/company/RegularImplementation/MainTest.java
@@ -1,5 +1,47 @@
 package com.company.RegularImplementation;
 
+import com.company.FlyweightPattern.Circle;
+import com.company.FlyweightPattern.ShapeFactory;
+
+import java.util.Random;
+
 public class MainTest
 {
+    public static final String colors[] = {"Red", "Green", "Blue", "White", "Black"};
+    
+    public static void main(String[] args)
+    {
+        //make new random variable
+        
+        //this number produces random int from 0 to given length exclusive, so add 1
+        //int randomNumber = rand.nextInt(colors.length) + 1;
+        
+        long startTime = System.currentTimeMillis();
+        //lav 100000000 objekter
+        for(int i=0; i < 500000; ++i) {
+            //her vil vi lave et nyt objekt
+            //men i stedet for at bruge new, så forespørger vi bare vores factory om det eksisterer
+            Circle circle = new Circle(colors[(int)((Math.random() * (6-1)))]);
+            //så setter man bare nogle random værdier på hvor objektet skal være i universet
+            circle.setX(getRandomX());
+            circle.setY(getRandomY());
+            //størrelse af cirklen
+            circle.setRadius(100);
+            //"tern" her printer vi bare til konsol
+            circle.draw();
+        }
+        
+        long endTime = System.currentTimeMillis();
+    
+        System.out.println("NO flyweight; that took: " +  (endTime-startTime)  + "ms");
+    }
+    private static String getRandomColor() {
+        return colors[(int)(Math.random()*colors.length)];
+    }
+    private static int getRandomX() {
+        return (int)(Math.random()*100 );
+    }
+    private static int getRandomY() {
+        return (int)(Math.random()*100);
+    }
 }
